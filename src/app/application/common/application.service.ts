f6c19a66ef8fb70ef924e718b35d66fade751d17
Feilong Zhu <feilongzhu1989@gmail.com>
2018-06-06 07:52:47 +0100

    fix #1829 Nothing happens when clicking "accept" (#1830)
    * fix #1829 Nothing happens when clicking "accept"
    
    * format

--- src/app/application/common/application.service.ts
+++ src/app/application/common/application.service.ts
@@ -62,7 +62,7 @@ export class ApplicationService {
     updateApplication(application: Application): Observable<Application> {
 
         return this.http
-            .post(applicationUrl, application, {
+            .put(applicationUrl, application, {
                 headers: new HttpHeaders()
                     .append('Authorization', `Bearer ${localStorage.getItem('access_token')}`)
             })
